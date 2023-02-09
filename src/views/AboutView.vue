c2482cefcf44ea9a556f9e7d655189519abfcf2b
trisanct <trisanct@gmail.com>
2023-02-09 13:02:02 +0800

    指示表格加载状态

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <h1>This is a test page</h1>
     <el-button @click="hello">测试服务器是否连通</el-button>
-    <h2>{{}}</h2>
+    <h2>{{msg}}</h2>
     
     
   </div>
@@ -12,15 +12,8 @@
   import { ref } from 'vue';
   const msg=ref('')
   const hello=()=>{
-    axios.get(`/server/Hello`,  { data:{
-      a:'2'
-    }
-
-    }).then((res)=>{
+    axios.get(`/server/Hello`,).then((res)=>{
       msg.value=res.data
     })
-  }
-  function formatDate(s: string) {
-    return `${s.slice(0,4)}年${s.slice(5,7)}月${s.slice(8,10)} ${s.slice(11,19)}`
   }
 </script>
--- src/views/Historys.vue
+++ src/views/Historys.vue
@@ -1,41 +1,61 @@
 <template>
   <h1>检测记录</h1>
-  <el-table :data="records" style="width: 100%">
+  <el-table v-loading="loading" :data="loading?nullvalue:records" style="width: 100%">
     <el-table-column prop="id" label="编号" width="180" />
     <el-table-column prop="mode" label="检测模式" width="180" />
     <el-table-column prop="time" label="提交时间" width="180" />
     <el-table-column prop="result" label="结果" />
   </el-table>
-    <el-pagination layout="prev, pager, next" v-model:current-page="id" :total="count" />
+  <el-pagination layout="prev, pager, next" v-model:current-page="id" :total="count" />
 </template>
 <script setup lang="ts">
 import axios from 'axios'
 import { onMounted, ref, watch } from 'vue'
 const count = ref(0)
 const id = ref(1)
-watch(id,async(newid,oldid)=>{
-  const res=await getpage(newid>oldid,newid>oldid?records.value[records.value.length-1].id:records.value[0].id,Math.abs(newid-oldid)-1)
-  records.value=res
+const loading = ref(true)
+watch(id, async (newid, oldid) => {
+  loading.value = true
+  const res = await getpage(
+    newid > oldid,
+    newid > oldid ? records.value[records.value.length - 1].id : records.value[0].id,
+    Math.abs(newid - oldid) - 1
+  )
+  records.value = res
+  loading.value = false
 })
-interface record{
-  id:number,
-  mode:string,
-  time:string,
-  result:string
+interface record {
+  id: number
+  mode: string
+  time: string
+  result: string
 }
 const records = ref<record[]>([])
-const getpage = async (direction:boolean,lastid:number,step:number) => {
+const nullvalue = [
+  { id: 0, mode: '', time: '', result: '' },
+  { id: 0, mode: '', time: '', result: '' },
+  { id: 0, mode: '', time: '', result: '' },
+  { id: 0, mode: '', time: '', result: '' },
+  { id: 0, mode: '', time: '', result: '' },
+  { id: 0, mode: '', time: '', result: '' },
+  { id: 0, mode: '', time: '', result: '' },
+  { id: 0, mode: '', time: '', result: '' },
+  { id: 0, mode: '', time: '', result: '' },
+  { id: 0, mode: '', time: '', result: '' },
+]
+const getpage = async (direction: boolean, lastid: number, step: number) => {
   return (await axios.get(`/server/History/${direction}/${lastid}/${step}`)).data
 }
-onMounted( async()=>{
-  let res=await getpage(true,0,0)
-  records.value=res.recordlist
-  count.value=res.count as number
+onMounted(async () => {
+  let res = await getpage(true, 0, 0)
+  records.value = res.recordlist
+  count.value = res.count as number
+  loading.value = false
 })
 </script>
 
 <style>
-.el-pagination{
+.el-pagination {
   justify-content: center;
 }
 </style>
