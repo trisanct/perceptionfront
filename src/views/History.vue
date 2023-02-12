94fce0d65600afd6ebeb26c361c85d3615ca077a
trisanct <trisanct@gmail.com>
2023-02-12 19:31:20 +0800

    2

--- src/views/History.vue
+++ src/views/History.vue
@@ -1,22 +1,18 @@
 <template>
   <h1>检测记录详情 #{{$route.params.id}}</h1>
-  <el-image src="/input/1.png"></el-image>
-  <el-row>
+  <el-row v-for="(r, i) in row" style="margin-bottom: 10px;">
     <el-col
-      v-for="(o, index) in 4"
-      :key="o"
+      v-for="(c, i) in 4"
       :span="5"
-      :offset="index > 0 ? 1 : 0"
+      :offset="i > 0 ? 1 : 0"
     >
       <el-card :body-style="{ padding: '0px' }">
-        <img
-          src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-          class="image"
-        />
+        <img :src="results[i].url" class="image" fit="contain" />
         <div style="padding: 14px">
-          <span>Corn Leaf Blight</span>
+          <div>{{results[i].filename}}</div>
+          <div>{{c}} {{i}}</div>
           <div class="bottom">
-            0.61
+            {{results[i].rate}}
           </div>
         </div>
       </el-card>
@@ -28,13 +24,24 @@ import { ref, onMounted } from 'vue'
 onMounted(() => {
   
 })
+interface result{
+  filename:string,
+  url:string,
+  disease:string,
+  rate:number
+}
+const results = ref<result[]>([])
+results.value.push({filename:'1.png',url:'/input/1.png',disease:'sick', rate:0.6})
+results.value.push({filename:'2.bmp',url:'/input/2.bmp',disease:'sick', rate:0.6})
+results.value.push({filename:'3.bmp',url:'/input/3.bmp',disease:'sick', rate:0.6})
+results.value.push({filename:'4.bmp',url:'/input/4.bmp',disease:'sick', rate:0.6})
+results.value.push({filename:'2.bmp',url:'/input/2.bmp',disease:'sick', rate:0.6})
+results.value.push({filename:'3.bmp',url:'/input/3.bmp',disease:'sick', rate:0.6})
+results.value.push({filename:'4.bmp',url:'/input/4.bmp',disease:'sick', rate:0.6})
+results.value.push({filename:'1.png',url:'/input/1.png',disease:'sick', rate:0.6})
+const row = ref(2)
 </script>
 <style scoped>
-.time {
-  font-size: 12px;
-  color: #999;
-}
-
 .bottom {
   margin-top: 13px;
   line-height: 12px;
@@ -42,7 +49,6 @@ onMounted(() => {
   justify-content: space-between;
   align-items: center;
 }
-
 .image {
   width: 100%;
   display: block;
